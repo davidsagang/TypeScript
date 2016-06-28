2e43d055b3e603b7301805acfe3ce6b123e5b438
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2016-06-28 14:40:06 -0700

    Wip- error when passing undefined to parameter destructuring

---
+++ tests/cases/conformance/es6/destructuring/destructuringParameterDeclaration8ES5.ts
@@ -0,0 +1,30 @@
+﻿// @target: es5
+
+function one({}, {foo, bar}) {
+  // ...
+}
+
+function two([]) {}
+
+function three({} = {}) {}
+
+function four([] = []) {}
+
+// should be an error
+one(undefined, { foo: 'foo', bar: 'bar' });  
+
+one(null,  { foo: 'foo', bar: 'bar' });
+
+two(undefined);
+
+two(null);
+
+// should not be an error
+
+three(undefined);
+
+three(null);
+
+four(undefined);
+
+four(null)
